2d8a4f26c12f5f635778760b6f3c863b7a0fed73
marco <g.marcotaba@gmail.com>
2021-04-30 17:38:38 +0200

    handle all other get requests

--- server/index.js
+++ server/index.js
@@ -1,13 +1,24 @@
+const path = require('path');
 const express = require("express");
 
 const PORT = process.env.PORT || 3001;
 
 const app = express();
 
-//If user make a GET call to API, we return a string
+
+//Node serve the files for our built React app
+app.use(express.static(path.resolve(__dirname, '../client/build')));
+
+
+//Handle GET requests to /api route, return a string
 app.get("/api", (req, res) => {
     res.json({ message: "Hello from server!"});
 });
+
+//All other get req not handled will return our React app
+app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, '../client/build', 'index.html'));
+})
 
 //Express create a web server for us on port 3001 if no value is given for variable PORT Heroku will set this value for us
 app.listen(PORT, () => {
