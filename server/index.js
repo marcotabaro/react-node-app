d5b21b8c540a987ccc42158d9b4ddac3befbf666
marco <g.marcotaba@gmail.com>
2021-04-30 16:24:58 +0200

    get call

--- server/index.js
+++ server/index.js
@@ -4,7 +4,14 @@ const PORT = process.env.PORT || 3001;
 
 const app = express();
 
+//If React make a GET call, we return a string
+app.get("/api", (req, res) => {
+    res.json({ message: "Hello from server!"});
+});
+
+//Express create a web server for us on port 3001 if no value is given for variable PORT Heroku will set this value for us
 app.listen(PORT, () => {
     console.log(`Server listening on ${PORT}`);
 });
+
 
